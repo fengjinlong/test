5c333a9acd02d129bdcd36be9e4fdba7019468db
fengjinlong <18618166564@163.com>
2019-01-10 17:22:20 +0800

    ad

---
+++ dll/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuex from 'vuex'
+import ElementUI from 'element-ui'
+import axios from 'axios'
+import vueRouter from 'vue-router'
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
